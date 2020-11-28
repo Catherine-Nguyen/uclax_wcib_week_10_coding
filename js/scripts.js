608a2f691f225715b83e6f1c41d5ef6307c51e53
Catherine <cat.nguyen520@gmail.com>
2020-11-28 15:32:28 -0800

    v5

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 particlesJS("particles-js", {
   "particles": {
     "number": {
@@ -22,7 +23,7 @@ particlesJS("particles-js", {
       "image": {
         "src": "img/github.svg",
         "width": 100,
-        "height": 100
+        "height": 450,
       }
     },
     "opacity": {
